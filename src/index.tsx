745ce8d36d50efc5b1b74d40ba18b215e1286f9a
WK <a.yambulatov@yandex.ru>
2025-05-03 17:39:33 +0400

    init

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './components/app/app';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
- 
+
 root.render(
   <React.StrictMode>
     <App />
